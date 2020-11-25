18b9968c98b9eba35f43d24c72fbc5b11ca1e826
Jorge Alejandro Gareca <e-jgareca@ripley.com>
2020-11-25 18:34:13 -0300

    init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Login from './component/Login';
+import './component/styles/styles.css'
+
+function App() {
+  return (
+    <div >
+      <Login/>
+    </div>
+  );
+}
+export default App;
